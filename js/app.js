066284f5a06ab11070564addda50eb64a43cf2e8
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2021-05-12 00:54:34 +0300

    projects page responsiveness done

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ function toggleMenu() {
 // contact: rocket and star animation
 
 function stars (){
- let count = 50;
+ let count = 40;
  let scene = document.querySelector('.scene');
  let i =0;
  while(i < count){
